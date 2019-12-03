dc6126eeacce286eb9e0076af9ed76c9617e9043
ikzsl <ikzsl@yandex.ru>
2019-12-04 00:22:14 +0300

    Добавляет базовую сборку

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 import '../scss/style.scss';
 
-console.log('Works!');
+let brands = document.querySelector('.brands');
+let brandsList = brands.querySelector('.brands__list');
+let brandsUnrollButton = brands.querySelector('.unroll');
+let toggleList = function () {
+  brandsList.classList.toggle('brands__list--rolled');
+
+  brandsUnrollButton.classList.toggle('unroll--rolled');
+  if (brands.querySelector('.brands__list--rolled')) {
+    brandsUnrollButton.textContent = 'Показать все';
+
+  } else {
+    brandsUnrollButton.textContent = 'Скрыть';
+  }
+};
+brandsUnrollButton.addEventListener('click', toggleList);
