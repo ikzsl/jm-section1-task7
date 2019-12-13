3130ecf84b233d2a69e2dd63f4c5e3dce82e7535
ikzsl <ikzsl@yandex.ru>
2019-12-13 03:57:33 +0300

    Добавляет интерактив

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,32 @@
 import '../scss/style.scss';
 import Swiper from 'swiper';
 
+
+// Сворачиваем-разворачиваем блок текста content__desc
+
+let content = document.querySelector('.content__desc');
+let contentText = content.querySelector('.content__desc-wrapper');
+let contentTextWrapper = content.querySelector('.content__text-wrapper');
+let contentUnrollButton = content.querySelector('.content__unroll');
+let toggleContent = function () {
+  contentText.classList.toggle('content__desc-wrapper--rolled');
+  contentTextWrapper.classList.toggle('content__text-wrapper--rolled');
+
+  contentUnrollButton.classList.toggle('unroll--rolled');
+  if (content.querySelector('.content__desc-wrapper--rolled')) {
+    contentUnrollButton.textContent = 'Читать далее';
+  } else {
+    contentUnrollButton.textContent = 'Скрыть';
+  }
+};
+contentUnrollButton.addEventListener('click', toggleContent);
+
+
+// Сворачиваем-разворачиваем блок brands
+
 let brands = document.querySelector('.brands');
 let brandsList = brands.querySelector('.brands__list');
-let brandsUnrollButton = brands.querySelector('.unroll');
+let brandsUnrollButton = brands.querySelector('.brands___unroll');
 let toggleBrandsList = function () {
   brandsList.classList.toggle('brands__list--rolled');
 
@@ -16,21 +39,42 @@ let toggleBrandsList = function () {
 };
 brandsUnrollButton.addEventListener('click', toggleBrandsList);
 
+
+// Сворачиваем-разворачиваем блок repair-types
+
+let repairTypes = document.querySelector('.repair-types');
+let repairTypesList = repairTypes.querySelector('.repair-types__list');
+let repairTypesUnrollButton = repairTypes.querySelector('.repair-types___unroll');
+let toggleRepairTypes = function () {
+  repairTypesList.classList.toggle('repair-types__list--rolled');
+
+  repairTypesUnrollButton.classList.toggle('unroll--rolled');
+  if (repairTypes.querySelector('.repair-types__list--rolled')) {
+    repairTypesUnrollButton.textContent = 'Показать все';
+  } else {
+    repairTypesUnrollButton.textContent = 'Скрыть';
+  }
+};
+repairTypesUnrollButton.addEventListener('click', toggleRepairTypes);
+
 // Показ-скрытие бокового меню
 let menuButton = document.querySelector('.main-header__menu-button');
 let navBar = document.querySelector('.navbar');
 let closeButton = navBar.querySelector('.navbar__close-button');
+let popupLayer = document.querySelector('.popup__layer');
 
 let showNavbar = function () {
   navBar.classList.toggle('navbar--on');
   menuButton.removeEventListener('click', showNavbar);
   closeButton.addEventListener('click', hideNavbar);
+  popupLayer.classList.remove('popup__layer--off');
 }
 
 let hideNavbar = function () {
   navBar.classList.toggle('navbar--on');
   closeButton.removeEventListener('click', hideNavbar);
   menuButton.addEventListener('click', showNavbar);
+  popupLayer.classList.add('popup__layer--off');
 }
 
 menuButton.addEventListener('click', showNavbar);
